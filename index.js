506b4df720e42a6c12a7a64a5c699546d5310f47
DGKN <sakardogukan@gmail.com>
2023-11-15 14:33:27 +0300

    index.js changed

--- index.js
+++ index.js
@@ -1,53 +1,46 @@
 "use strict"
 
-/* ------------ INDEX.JS----------------- */
-
-/* --------------------------------------- *
-$ npm init -y
-$ npm i express dotenv express-async-error
-$ npm i mongoose
-$ echo PORT=8000 > .env
-$ echo MONGODB=mongodb://localhost:27017/ >> .env
------------------------------------------- */
+/* --------------------INDEX.JS------------------------
+                BLOG_API_APP PROJECT
+------------------------------------------------------- */
 
 const express = require('express')
 const app = express()
 
 require('dotenv').config()
-// const HOST = process.env.HOST || "127.0.0.1"
 const PORT = process.env.PORT || 8000
 
-/* --------------------------------------- */
-// SessionCookies:
-
-const session = require('cookie-session')
-
+/* ------------------------------------------------------- */
+//! Cookie-Session:
+const session = require("cookie-session")
 app.use(session({
     secret: process.env.SECRET_KEY || 'secret_keys_for_cookies',
-    //name:'cookie', // default: req.session
-    // maxAge: 1000 * 60 * 60 * 24 // 1 day(miliseconds)
+    // name: 'cookie', // default: req.session
+    // maxAge: 1000 * 60 * 60 * 24 // 1 day (miliseconds)
 }))
-
-
 
 app.use(express.json())
 
-//DB Connection
+//! DB Connection
 require('./src/dbConnection')
 
-// Home Page:
+//! Searching & Sorting & Pagination
+app.use(require('./src/middlewares/findSearchSortPage'))
+
+//! Home Page
 app.all('/', (req, res) => {
-    res.send('Welcome To Home Page - Blog API')
+    res.send('WELCOME TO BLOG API')
 })
 
-//Routes
+//! Routes:
 app.use('/user', require('./src/routes/userRoute'))
 app.use('/blog', require('./src/routes/blogRoute'))
 
-// Synchronization: require edilen dosya sync işlemi de fonksiyon olmalıdır.
+//! Synchronisation:
 // require('./src/sync')()
 
-//Error Handler:
+//! Errorhandler
 app.use(require('./src/errorHandler'))
 
+//! Port Listen
 app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
